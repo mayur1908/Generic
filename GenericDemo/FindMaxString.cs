3215668d5cb6dcc1053012515dcaed3fe4ca79f2
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-05-13 11:03:44 +0530

    String File

---
+++ GenericDemo/FindMaxString.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericDemo
+{
+    public class FindMaxString
+    {
+        public static string MaxString(string str1, string str2, string str3)
+        {
+            if (str1.CompareTo(str2) > 0 && str1.CompareTo(str3) > 0 || str1.CompareTo(str2) >= 0 && str1.CompareTo(str3) > 0 || str1.CompareTo(str2) > 0 && str1.CompareTo(str3) >= 0)
+            {
+                return str1;
+            }
+            if (str2.CompareTo(str1) > 0 && str2.CompareTo(str3) > 0 || str2.CompareTo(str1) >= 0 && str2.CompareTo(str3) > 0 || str2.CompareTo(str2) > 0 && str2.CompareTo(str3) >= 0)
+            {
+                return str2;
+            }
+            if (str3.CompareTo(str1) > 0 && str3.CompareTo(str2) > 0 || str3.CompareTo(str1) >= 0 && str3.CompareTo(str2) > 0 || str3.CompareTo(str2) > 0 && str3.CompareTo(str2) >= 0)
+            {
+                return str3;
+            }
+            return str1;
+        }
+    }
+}
