c4446c92b0eca2f941951b06cb3ee3bcb7c0967e
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-05-13 11:01:45 +0530

    Float File

---
+++ GenericDemo/FindMaxDouble.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    public class FindMaxDouble
+    {
+        public static double MaxNumber(double num1, double num2, double num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 || num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 || num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+            {
+                return num1;
+            }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 || num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 || num2.CompareTo(num2) > 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num2;
+            }
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 || num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 || num3.CompareTo(num2) > 0 && num3.CompareTo(num2) >= 0)
+            {
+                return num3;
+            }
+            return num1;
+
+        }
+    }
+}
